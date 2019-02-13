edb4cc708aba768751ba7f4a83faeb697e61e90b
Dhyo-Ryuu <dhyojr11@gmail.com>
2019-02-14 01:23:44 +0700

    JS3-Tgs2-Angular

---
+++ praktikum3-tugas2/src/app/ryuu/ryuu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ryuu',
+  templateUrl: './ryuu.component.html',
+  styleUrls: ['./ryuu.component.css']
+})
+export class RyuuComponent implements OnInit {
+
+title='3authors';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
